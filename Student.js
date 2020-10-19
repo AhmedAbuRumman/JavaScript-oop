a6bf9f9ec5812174129356a6c938e188956165b4
AhmedAbuRumman <ahmed.e.aburuman>
2020-10-19 13:42:08 +0300

    commit

---
+++ Student.js
@@ -0,0 +1,10 @@
+Class Student {
+    constructor (Id ,Name , Email , mobile_number){
+        this.Id = Id ;
+        this.Name = Name ;
+        this.Email = Email ;
+        this.mobile_number = mobile_number ;
+
+    }
+}
+let Student1 = new Student ('123443','Sara','sara@orange.com','0777777777')
---
+++ Teacher.js
@@ -0,0 +1,8 @@
+class Teacher extends Student {
+    constructor(Id, Name, Email, mobile_number, Salary, subjects){
+        super(Id, Name, Email, mobile_number)
+        this.Salary = Salary;
+        this.subjects = subjects;
+    }
+}
+let Teacher1 = new Teacher ('196432','Sadi','Sadi@orange.com',0777788888,800,'[English, Arabic, Math, science]')
